0fe1d63f24aabd9231832058015e07a5d8836479
Jeremy Skrdlant <jeremy.skrdlant@gmail.com>
2023-09-14 19:19:44 -0600

    Initial Commit

---
+++ YouCanPutThatBracketThere!/YouCanPutThatBracketThere_App.swift
@@ -0,0 +1,17 @@
+//
+//  YouCanPutThatBracketThere_App.swift
+//  YouCanPutThatBracketThere!
+//
+//  Created by admin on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct YouCanPutThatBracketThere_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
